2bbd9359092fb3684b84ece5b64f040db52a9a76
Dominik Gałenza <dominik.galenza@gmail.com>
2022-06-29 18:57:40 +0200

    Implemented ignoring dead enemies

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -54,6 +54,13 @@ namespace RPG.Combat
             return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
         }
 
+        public bool CanAttack(CombatTarget combatTarget)
+        {
+            if (combatTarget == null) return false;
+            Health targetToTest = GetComponent<Health>();
+            return targetToTest != null && !targetToTest.IsDead();
+        }
+
         public void Attack(CombatTarget combatTarget)
         {
             print("Don't be afraid, soon you'll be in a better place");
