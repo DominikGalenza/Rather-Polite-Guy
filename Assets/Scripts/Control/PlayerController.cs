7624ac1d0379d9b45e1385d35477504e318adb84
Dominik Gałenza <dominik.galenza@gmail.com>
2022-06-30 19:55:47 +0200

    Disabled moving when a character is dead

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 using RPG.Movement;
 using System;
 using RPG.Combat;
+using RPG.Core;
 
 namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
+        Health health;
+
+        private void Start() 
+        {
+            health = GetComponent<Health>();
+        }
+
         void Update()
         {
+            if (health.IsDead()) return;
+            
             if (InteractWithCombat()) return;
             if (InteractWithMovement()) return;
             print("Your physical limitations don't allow you to go there");
