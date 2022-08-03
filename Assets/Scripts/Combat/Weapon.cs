c91a5e98cb9a4525a1bd1219db33aed8b51a063a
Dominik Gałenza <dominik.galenza@gmail.com>
2022-08-03 18:29:33 +0200

    Implemented a health progression based on the level

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,6 +1,5 @@
-using System;
-using RPG.Core;
 using UnityEngine;
+using RPG.Attributes;
 
 namespace RPG.Combat
 {
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using RPG.Core;
 using UnityEngine;
 using UnityEngine.AI;
 using RPG.Saving;
+using RPG.Attributes;
 
 namespace RPG.Movement
 {
