2071a63c8d42e00bebbf680493234063f12cdb61
Dominik Gałenza <dominik.galenza@gmail.com>
2022-08-03 21:44:30 +0200

    Added health display for enemies

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -41,12 +41,19 @@ namespace RPG.Combat
                 AttackBehaviour();
             }
         }
+
         public void EquipWeapon(Weapon weapon)
         {
             currentWeapon = weapon;
             Animator animator = GetComponent<Animator>();
             weapon.Spawn(rightHandTransform, leftHandTransform, animator);
         }
+
+        public Health GetTarget()
+        {
+            return target;
+        }
+
         private void AttackBehaviour()
         {
             transform.LookAt(target.transform);
@@ -57,11 +64,13 @@ namespace RPG.Combat
                 timeSinceLastAttack = 0;
             }
         }
+
         private void TriggerAttack()
         {
             GetComponent<Animator>().ResetTrigger("stopAttack");
             GetComponent<Animator>().SetTrigger("attack");
         }
+
         // Animation Event
         void Hit()
         {
@@ -76,25 +85,30 @@ namespace RPG.Combat
                 target.TakeDamage(currentWeapon.GetDamage());
             }
         }
+
         void Shoot()
         {
             Hit();
         }
+
         private bool GetIsInRange()
         {
             return Vector3.Distance(transform.position, target.transform.position) < currentWeapon.GetRange();
         }
+
         public bool CanAttack(GameObject combatTarget)
         {
             if (combatTarget == null) { return false; }
             Health targetToTest = combatTarget.GetComponent<Health>();
             return targetToTest != null && !targetToTest.IsDead();
         }
+
         public void Attack(GameObject combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
             target = combatTarget.GetComponent<Health>();
         }
+        
         public void Cancel()
         {
             StopAttack();
