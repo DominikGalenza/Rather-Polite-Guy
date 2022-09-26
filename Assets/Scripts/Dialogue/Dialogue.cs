7f449aa9ce4d53c2711bdbd8b8070ddf1f791052
Dominik Gałenza <dominik.galenza@gmail.com>
2022-09-26 20:14:19 +0200

    Added a default node to the dialogue SO

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -2,8 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
-public class Dialogue : ScriptableObject 
+namespace RPG.Dialogue
 {
-    [SerializeField] DialogueNode[] nodes;
+    [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
+    public class Dialogue : ScriptableObject 
+    {
+        [SerializeField] List<DialogueNode> nodes = new List<DialogueNode>();
+
+#if UNITY_EDITOR
+        private void Awake() 
+        {
+            if (nodes.Count == 0)
+            {
+                nodes.Add(new DialogueNode());
+            }
+        }
+#endif
+
+        public IEnumerable<DialogueNode> GetAllNodes()
+        {
+            return nodes;
+        }
+    }
 }
---
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace RPG.Dialogue.Editor
+{
+    public class DialogueEditor : EditorWindow
+    {
+        Dialogue selectedDialogue = null;
+
+        [MenuItem("Window/Dialogue Editor")]
+        public static void ShowEditorWindow()
+        {
+            GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
+        }
+
+        [OnOpenAsset(1)]
+        public static bool OnOpenAsset(int instanceID, int line)
+        {
+            Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
+            if (dialogue != null)
+            {
+                ShowEditorWindow();
+                return true;
+            }
+            return false;
+        }
+
+        private void OnEnable() 
+        {
+            Selection.selectionChanged += OnSelectionChanged;
+        }
+
+        private void OnSelectionChanged()
+        {
+            Dialogue newDialogue = Selection.activeObject as Dialogue;
+
+            if (newDialogue != null)
+            {
+                selectedDialogue = newDialogue;
+                Repaint();
+            }
+        }
+
+        private void OnGUI() 
+        {
+            if (selectedDialogue == null)
+            {
+                EditorGUILayout.LabelField("No dialogue selected");
+            }
+            else
+            {
+                foreach (DialogueNode node in selectedDialogue.GetAllNodes())
+                {
+                    EditorGUILayout.LabelField(node.text);
+                }
+            }
+        }
+    }
+}
